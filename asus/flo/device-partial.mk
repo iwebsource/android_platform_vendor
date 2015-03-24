a4afcc44a34e78a02adf1640eefeb1f2924328f3
Adam Stewart <iwebsource1@gmail.com>
2015-03-23 23:12:30 -0300

    Test Branch lollipop-5.1

--- asus/flo/device-partial.mk
+++ asus/flo/device-partial.mk
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# TEST BRANCH LOLLIPOP-5.1
 # Asus blob(s) necessary for Flo hardware
 PRODUCT_COPY_FILES := \
     vendor/asus/flo/proprietary/libacdbdata.so:system/lib/libacdbdata.so:asus \
